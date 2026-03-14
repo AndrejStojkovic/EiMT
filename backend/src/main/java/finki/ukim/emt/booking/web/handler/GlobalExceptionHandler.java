080a782e04931f0a3933ebc1ee130acc0fc78c20
Andrej Stojkovikj <andrejstojkovikj@yahoo.com>
2026-03-14 20:11:45 +0100

    Added a global exception handler

---
+++ backend/src/main/java/finki/ukim/emt/booking/web/handler/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package finki.ukim.emt.booking.web.handler;
+
+import finki.ukim.emt.booking.web.dto.ApiError;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handleException(Exception e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiError.of(HttpStatus.BAD_REQUEST, e.getMessage()));
+    }
+}
